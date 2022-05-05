4367c88fc9d2f0d51514a70c6432b17c7c8f01d5
Paulo-Sergio-Oliveira <99352391+Paulo-Sergio-Oliveira@users.noreply.github.com>
2022-05-05 11:18:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let nao = 0;
 function FSim(){
     sim = sim + 1;
     document.getElementById("sim").innerHTML = sim;
-    document.getElementById("sim").style.width = sim+"0px";
+    document.getElementById("sim").style.width = sim+sim+"0px";
 }
 function FNao(){
     nao = nao + 1;
     document.getElementById("nao").innerHTML = nao;
-    document.getElementById("nao").style.width = nao+"0px";
+    document.getElementById("nao").style.width = nao+nao+"0px";
 }
